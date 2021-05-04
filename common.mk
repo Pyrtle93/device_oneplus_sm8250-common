8befb96a9fd489a9214bb1f9e56f3d66adbb7a6f
chandu078 <chandudyavanapelli03@gmail.com>
2021-05-04 13:33:42 +0530

    sm8250-common: Drop QTI vibrator HAL Revert "sm8250-common: Import QTI vibrator HAL"
    This reverts commit dc3d086fbd42d5108eb73e1f3405eeb979003cb5.
    
    Revert "sm8250-common: vibrator: Implement OOS style effects for LED vibrator"
    
    This reverts commit 04cf04fab5d6b09dcc4da25d16bd10326d64a9b7.
    
    Revert "sm8250-common: vibrator: Sync with upstream"
    
    This reverts commit 86b4e9c7076a0c305084b788d42da7534fe5b75c.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -498,7 +498,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.oneplus_kona
+    vendor.qti.hardware.vibrator.service
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
