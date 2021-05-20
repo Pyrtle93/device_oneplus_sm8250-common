80911096ef8120ff94940cb9eb52b43747acbc8c
Pyrtle93 <xxpyrtlexx93@gmail.com>
2021-05-20 19:16:20 -0400

    Revert "sm8250-common: overlay: Enable SmartCharging"
    derp has no intentions of adding it atm so let's drop it for now.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -218,14 +218,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.smartcharging.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.smartcharging.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := vendor.oem_ftm.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
